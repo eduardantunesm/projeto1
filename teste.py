ea9d4e9715ef99c1567cabf8daf3348c5162e76d
eduardantunesm <eduardantunesm@gmail.com>
2026-04-28 15:41:30 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,11 +5,11 @@ st.write("Esse site foi criado pela Eduarda Antunes com o intuito de apresentar 
 
 st.subheader("FATO 1: ACIDENTE DO CÉSIO 137 EM GOIÂNIA ")
 st.write("O acidente com o Césio-137 em Goiânia, ocorrido em setembro de 1987, foi o maior acidente radiológico do mundo fora de uma usina nuclear. Após a violação de uma máquina de radioterapia abandonada, o pó azul brilhante (cloreto de césio) causou a contaminação direta de centenas de pessoas, resultando em 4 mortes confirmadas inicialmente, milhares de toneladas de rejeitos e um grande estigma social para a cidade.") 
-st.write("A Origem: Catadores de lixo encontraram um aparelho de radioterapia abandonado no Instituto Goiano de Radioterapia (IGR) e o venderam para um ferro-velho.") 
-st.write("O Agente: O equipamento continha Cloreto de Césio, uma fonte radioativa que, ao ser liberada, exibia um pó azul brilhante.") 
-st.write("Dispersão, vítimas e sintomas: Devido ao manuseio e curiosidade, o material foi espalhado por várias casas e locais públicos, contaminando o ambiente e pessoas. Os primeiros sintomas incluíram náuseas, tonturas e queimaduras na pele. Quatro pessoas morreram inicialmente.") 
-st.write("Consequências: O episódio gerou cerca de 6 mil toneladas de rejeitos, que hoje estão armazenados em um depósito em Abadia de Goiás, onde permanecerão radioativos por 300 anos.") 
-st.write("Legado: O caso transformou o Brasil em referência científica mundial em segurança radiológica, mas os sobreviventes ainda enfrentam sequelas físicas e o estigma social.") 
+st.write("**A Origem:** Catadores de lixo encontraram um aparelho de radioterapia abandonado no Instituto Goiano de Radioterapia (IGR) e o venderam para um ferro-velho.") 
+st.write("**O Agente:** O equipamento continha Cloreto de Césio, uma fonte radioativa que, ao ser liberada, exibia um pó azul brilhante.") 
+st.write("**Dispersão, vítimas e sintomas:** Devido ao manuseio e curiosidade, o material foi espalhado por várias casas e locais públicos, contaminando o ambiente e pessoas. Os primeiros sintomas incluíram náuseas, tonturas e queimaduras na pele. Quatro pessoas morreram inicialmente.") 
+st.write("**Consequências:** O episódio gerou cerca de 6 mil toneladas de rejeitos, que hoje estão armazenados em um depósito em Abadia de Goiás, onde permanecerão radioativos por 300 anos.") 
+st.write("**Legado:** O caso transformou o Brasil em referência científica mundial em segurança radiológica, mas os sobreviventes ainda enfrentam sequelas físicas e o estigma social.") 
 
 st.image("https://linhadotempo.mpf.mp.br/www/linha-do-tempo-pr-go-docs/linha-do-tempo-pr-go-imagens/CRCN-CO%20-%206%20-%20Descontaminacao%20sendofeita%20na%20Rua%2057.jpg/@@images/a145c706-c853-4573-bf2d-cb8c56d67a2d.jpeg", caption="Trabalhos de descontaminação dos rejeitos radioativos do Césio 137 narua 57, região central de Goiânia.")
 
@@ -17,4 +17,5 @@ st.image("https://static.dw.com/image/40458019_605.jpg", caption="Cabeçote enco
 
 st.image("https://www3.unicentro.br/petfisica/wp-content/uploads/sites/54/2015/10/Goiania_1987.10.10_corte.jpg", caption="Jornal: Goiânia ficou exposta à radiação por 16 dias.")
 
-st.video("https://www.youtube.com/watch?v=tF7G0BFXEcQ", caption="Notícia sobre o acidente.")
+st.video("https://www.youtube.com/watch?v=tF7G0BFXEcQ")
+
