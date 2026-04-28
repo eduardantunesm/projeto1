7a3d13f960297c6959b35486e5452c7805f77488
eduardantunesm <eduardantunesm@gmail.com>
2026-04-28 15:22:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-st.title("Teste ECMI 2")
-st.write("Esse é o meu texto") 
+st.title("Caixa do Tempo: histórias do Brasil")
+st.write("Esse site foi criado com o intuito de apresentar acontecimentos históricos no território brasileiro.") 
+
+st.write("Fato 1: acidente do césio 137 em Goiânia)
+         st.image("https://linhadotempo.mpf.mp.br/www/linha-do-tempo-pr-go-docs/linha-do-tempo-pr-go-imagens/CRCN-CO%20-%206%20-%20Descontaminacao%20sendofeita%20na%20Rua%2057.jpg/@@images/a145c706-c853-4573-bf2d-cb8c56d67a2d.jpeg")
