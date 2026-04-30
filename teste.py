e021742baedd16cf0a97d389556f4590b19cb2b4
eduardantunesm <eduardantunesm@gmail.com>
2026-04-30 18:10:50 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -23,10 +23,10 @@ st.subheader("FATO 2: INCÊNDIO DO MUSEU NACIONAL (2018) ")
 st.write("200 anos de história viraram cinzas em uma noite. Na noite de 2 de setembro de 2018, o Brasil e o mundo assistiram, impotentes, às chamas que consumiram o Palácio de São Cristóvão, na Quinta da Boa Vista, Rio de Janeiro. O incêndio no Museu Nacional não destruiu apenas um edifício histórico; ele devorou um dos acervos científicos e antropológicos mais importantes do Hemisfério Sul.")
 st.write("**O tamanho da perda:**")
 st.write("O fogo comprometeu cerca de 90% das 20 milhões de peças da instituição. Entre os itens irremediavelmente perdidos ou severamente danificados estavam:") 
-st.write("**Luzia:** O fóssil humano mais antigo das Américas (parcialmente recuperado dos escombros).")
-st.write("**Maxakalisaurus topai:** O esqueleto do primeiro dinossauro de grande porte montado no Brasil.")
-st.write("**Coleções Egípcias e Greco-romanas:** Peças raras que sobreviviam há milênios e sucumbiram ao calor.")
-st.write("**Registros Linguísticos:** Documentos únicos de línguas indígenas que já não possuem falantes vivos.")
+st.write("**Luzia:** o fóssil humano mais antigo das Américas (parcialmente recuperado dos escombros).")
+st.write("**Maxakalisaurus topai:** o esqueleto do primeiro dinossauro de grande porte montado no Brasil.")
+st.write("**Coleções Egípcias e Greco-romanas:** peças raras que sobreviviam há milênios e sucumbiram ao calor.")
+st.write("**Registros Linguísticos:** documentos únicos de línguas indígenas que já não possuem falantes vivos.")
 st.write("O Alerta Ignorado: a tragédia foi o ápice de décadas de negligência e falta de investimento. Problemas na rede elétrica e a ausência de um sistema de combate a incêndios eficiente transformaram o palácio — que já foi residência da família real — em uma armadilha de material inflamável. Hoje, o Museu Nacional vive um complexo processo de reconstrução. Embora as paredes externas estejam sendo restauradas e novas doações de acervos estejam chegando, o episódio permanece como uma ferida aberta na cultura brasileira, servindo de lembrete sobre a fragilidade da nossa memória institucional.")
 
 st.image("https://s2-g1.glbimg.com/_xTDev12tGt7pghbrXJnEF75woU=/0x0:3500x2412/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/J/m/pdxkp1SmeBBBr5A7q22A/2018-09-03t003119z-1651330244-rc168d791cc0-rtrmadp-3-brazil-fire-museum.jpg", caption="Foto mostra a tentativa de conter o incêndio no Museu Nacional no Rio de Janeiro — Foto: Reuters/Ricardo Moraes")
@@ -35,4 +35,51 @@ st.image("https://s2-g1.glbimg.com/nmE-H8aQFQGvnqy_BuXtysQ7jGU=/0x0:3500x2170/10
 
 st.image("https://s2-g1.glbimg.com/ZroS641sJs5bGeVWXq4APHndxY8=/0x0:783x462/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/M/t/iMVtBvSXaSvcbhKPUVpg/museu2.jpg", caption="Grande parte do Museu Nacional destruída pelas chamas — Foto: Reprodução/TV Globo")
 
-st.video("https://g1.globo.com/fantastico/video/incendio-destroi-o-museu-nacional-no-rio-de-janeiro-6991190.ghtml")
+st.video("https://www.youtube.com/watch?v=wJ-Ptbd2a64")
+
+st.subheader("FATO 3: DESCOBERTA DO PRÉ-SAL (2006)")
+st.write("Uma riqueza escondida a quilômetros de profundidade: descoberta do pré-sal foi um divisor de águas na história energética do Brasil, revelando uma das maiores províncias petrolíferas do mundo sob uma espessa camada de sal que chega a dois mil metros de profundidade.")
+st.write("**Um marco histórico**: embora as explorações tenham começado anos antes, o anúncio oficial que mudou o patamar do país ocorreu em 2006, com a descoberta do campo de Tupi (hoje Campo de Lula), na Bacia de Santos. A Petrobras demonstrou uma capacidade técnica inédita ao conseguir perfurar quase 7 mil metros — somando a lâmina d'água, o sedimento e a crosta de sal.")
+st.write("**Desafios e grandiosidade:** a extração nessa região foi considerada uma das fronteiras tecnológicas mais complexas da indústria moderna por três motivos principais:")
+st.write("**Profundidade Extrema:** operar sob pressões gigantescas e temperaturas oscilantes.")
+st.write("**A camada de sal**: atravessar uma camada rochosa que se comporta de maneira plástica, exigindo engenharia de ponta.")
+st.write("**Logística:** os campos situam-se a mais de 300 km da costa, o que exige uma infraestrutura massiva de transporte e suporte.")
+st.write("**Impacto econômico:**")
+st.write("O pré-sal não apenas garantiu a autossuficiência brasileira, mas transformou o país em um dos grandes exportadores de petróleo do globo. Além da produção de óleo bruto, a exploração impulsionou o desenvolvimento de tecnologias nacionais e gerou o Fundo Social, destinado a investir em áreas prioritárias como educação e saúde. Hoje, essa região responde pela maior parte da produção nacional, consolidando-se como o motor da indústria de energia no Brasil.")
+
+st.image("https://memorialdademocracia.com.br/publico/image/16653", caption="Angra dos Reis: cerimônia de batismo da plataforma P-52 com funcionários da Petrobras.")
+
+st.image("https://memorialdademocracia.com.br/publico/image/15394", caption="Tecnologia desenvolvida pela Petrobras permite explorar reservas perfurando duras camadas de sal.")
+
+st.video("https://www.youtube.com/watch?v=IvdDXljIiS8")
+
+st.subheader("FATO 4: BRASIL PENTACAMPEÃO MUNDIAL (2002) ") 
+st.write("O mundo inteiro viu o Brasil no topo mais uma vez em 2002. Naquela manhã de 30 de junho em Yokohama, no Japão, a Seleção Brasileira não apenas conquistava a Copa do Mundo, mas encerrava um ciclo de superação que parecia improvável anos antes.")
+st.write("**A campanha perfeita:** Sob o comando de Luiz Felipe Scolari, o "Família Scolari", o Brasil alcançou o título com uma performance impecável: 7 jogos e 7 vitórias. Foi a consagração do talento individual a serviço de um conjunto resiliente.")
+st.write("**Os três Rs:** o trio de ataque formado por **Ronaldo, Rivaldo e Ronaldinho Gaúcho** aterrorizou as defesas adversárias, somando 15 dos 18 gols marcados pela equipe.")
+st.write("**A redenção do fenômeno:** após as dúvidas sobre seu joelho e o drama de 1998, Ronaldo marcou os dois gols da final contra a Alemanha, tornando-se o artilheiro isolado da competição com 8 gols.")
+st.write("**O capitão do tri ao penta:** Cafu tornou-se o primeiro jogador a disputar três finais consecutivas de Copa do Mundo, erguendo a taça para selar a glória eterna.") 
+st.write("**O significado do título:** A vitória sobre a Alemanha por 2 a 0 na final marcou o primeiro encontro das duas potências em Copas e isolou o Brasil como o único Pentacampeão. Mais do que um troféu, 2002 devolveu ao povo brasileiro o orgulho do "futebol arte" aliado à competitividade, reafirmando o país como a maior potência do esporte no planeta. Desde então, o brilho das cinco estrelas no peito serve como o maior símbolo da identidade esportiva brasileira, lembrando o momento em que o planeta, do Japão à Coreia, se curvou ao talento canarinho.")
+
+st.image("https://imagens.globoradio.globo.com/cbn/fotos/uploads2015/89342/cd_vtres_copa_jpn_kor239jpg_610x340.jpg", caption="Cafú ergue a taça do Penta")
+
+st.image("https://jpimg.com.br/uploads/2017/06/749777164-ae-selecao-brasileira-de-2002-unica-campea-do-mundo-com-7-vitorias-em-7-jogos.jpg", caption="Seleção Brasileira de 2002: a única campeã do mundo com 7 vitórias em 7 jogos.")
+
+st.video("https://www.youtube.com/watch?v=AzXYGdyfV4o")
+
+st.subheader("FATO 5: CRIAÇÃO DO SUS (1988) ")
+st.write("Saúde gratuita como direito de todos oficialmente: foi com essa premissa revolucionária que a Constituição de 1988 deu vida ao Sistema Único de Saúde (SUS). Antes dele, o acesso à saúde no Brasil era restrito a quem contribuía com a Previdência Social, deixando milhões de brasileiros dependentes da caridade ou de serviços precários.")
+st.write("**O SUS foi fundamentado em três pilares principais que mudaram a face da cidadania no país:**")
+st.write("**Universalidade:** Todo e qualquer cidadão tem direito ao atendimento, sem distinção.")
+st.write("**Equidade:** o sistema busca oferecer mais a quem mais precisa, diminuindo as desigualdades.")
+st.write("**Integralidade:** cuidado vai desde a prevenção (vacinas) até procedimentos de altíssima complexidade (transplantes).")
+st.write("**A importância global: um modelo para o mundo**")
+st.write("O Brasil é o único país com mais de 200 milhões de habitantes que possui um sistema de saúde público, universal e totalmente gratuito. Essa escala torna o SUS uma referência internacional em diversas frentes. Mais do que um serviço médico, o SUS é um patrimônio social que garante que a vida de um cidadão não tenha um preço, consolidando-se como um dos maiores exemplos de política inclusiva e justiça social do mundo contemporâneo.")
+
+st.image("https://www.brasildefato.com.br/wp-content/uploads/2024/09/image_processing20200201-29235-zzvgfd.jpg", caption="Somente a partir de 5 de outubro de 1988 a Saúde passou a ser assegurada pela Constituição como direito universal | Crédito da Foto: Arquivo Agência Senado")
+
+st.image("https://aventurasnahistoria.com.br/wp-content/uploads/posto_de_saude_em_paratinga_1_janeiro_de_2017.jpg", captions=Posto de saúde em Paratinga, na Bahia, inaugurado em 1989 / Créditos: Wikimedia Commons")
+
+st.image(https://www.gov.br/saude/pt-br/assuntos/noticias/2023/marco/historia-do-ze-gotinha-saiba-como-nasceu-o-simbolo-da-imunizacao-do-brasil/design-sem-nome-62.png/@@images/4745f5cc-fbf8-476d-b7f6-4a69be1aed11.png", caption="Zé Gotinha: símbolo da imunização brasileira - Foto: Julia Prado/MS")
+
+st.image("https://www.youtube.com/watch?v=YJaEz2qBveY") 
